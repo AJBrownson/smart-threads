6fbf59b3ad66a3217ec5397749f66c0ac5e234d0
Anietie Brownson <anni.brownson@gmail.com>
2023-04-17 20:55:53 +0100

    feat: Add tabs and button in Customizer page

--- src/pages/Customizer.jsx
+++ src/pages/Customizer.jsx
@@ -1,19 +1,55 @@
-import { useState, ueEffect } from 'react'
-import { AnimatePresence, motion } from 'framer-motion'
-import { useSnapshot } from 'valtio'
+import { useState, ueEffect } from "react";
+import { AnimatePresence, motion } from "framer-motion";
+import { useSnapshot } from "valtio";
 
-import cnfig from "../config/config"
-import state from "../store"
-import { download } from "../assets"
+import cnfig from "../config/config";
+import state from "../store";
+import { download } from "../assets";
 
-import { downloadCanvasToImage, reader } from "../config/helpers.js"
-import { EditorTab, FilterTab, DecalTypes } from "../config/constants"
-import { fadeAnimation, slideAnimation } from "../config/motion"
+import { downloadCanvasToImage, reader } from "../config/helpers.js";
+import { EditorTabs, FilterTabs, DecalTypes } from "../config/constants";
+import { fadeAnimation, slideAnimation } from "../config/motion";
+
+import { AIPicker, FilePicker, ColorPicker, Tab, CustomButton } from "../components";
 
 const Customizer = () => {
+  const snap = useSnapshot(state);
   return (
-    <div>Customizer</div>
-  )
-}
+    <AnimatePresence>
+      {!snap.intro && (
+        <>
+          <motion.div
+            key="custom"
+            className="absolute top-0 left-0 z-10"
+            {...slideAnimation("left")}
+          >
+            <div className="flex items-center min-h-screen">
+              <div className="editortabs-container tabs">
+                {EditorTabs.map((tab) => (
+                  <Tab 
+                    key={tab.name} 
+                    tab={tab} 
+                    handleClick={() => {}} 
+                  />
+                ))}
+              </div>
+            </div>
+          </motion.div>
 
-export default Customizer
+          <motion.div className="absolute z-10 top-5 right-5"
+            {...fadeAnimation}
+          >
+            <CustomButton
+              type="filled"
+              title="Go Back"
+              handleClick={() => state.intro = true}
+              customStyles="w-fit px-4 py-2.5 font-bold text-sm"
+              />
+          </motion.div>
+        </>
+      )}
+    </AnimatePresence>
+  );
+};
+
+export default Customizer;
