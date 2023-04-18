982eca7b32c7c7e4d1ab0760aced7dd804e803c8
Anietie Brownson <anni.brownson@gmail.com>
2023-04-18 08:07:49 +0100

    bug: background rendering black instead of color after writing functionality for CamerRig, Backdrop and Shirt files

--- src/canvas/CamerRig.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const CamerRig = () => {
-  return (
-    <div>CamerRig</div>
-  )
-}
-
-export default CamerRig
--- src/canvas/index.jsx
+++ src/canvas/index.jsx
@@ -1,23 +1,27 @@
 import { Canvas } from '@react-three/fiber'
-import { Environment, Center } from "@react-three/drei"
+import { Environment, Center } from '@react-three/drei';
 
-import Backdrop from './Backdrop'
-import CamerRig from './CamerRig'
-import Shirt from "./Shirt"
-
+import Shirt from './Shirt';
+import Backdrop from './Backdrop';
+import CameraRig from './CameraRig';
 
 const CanvasModel = () => {
   return (
-    <Canvas>
+    <Canvas
+      shadows
+      camera={{ position: [0, 0, 0], fov: 25 }}
+      gl={{ preserveDrawingBuffer: true }}
+      className="w-full max-w-full h-full transition-all ease-in"
+    >
       <ambientLight intensity={0.5} />
       <Environment preset="city" />
 
-      {/* <CameraRig>
-        <Backdrop /> */}
+      <CameraRig>
+        <Backdrop />
         <Center>
           <Shirt />
         </Center>
-      {/* </CameraRig> */}
+      </CameraRig>
     </Canvas>
   )
 }
